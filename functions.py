a2616b457b3599ed6c0797e207cb9b19c793d8e5
btemovska <btemovska@gmail.com>
2020-09-06 22:36:06 -0400

    intro to functions

---
+++ functions.py
@@ -0,0 +1,14 @@
+def multiply():
+    result = 10.5 * 4
+    return result
+
+
+answer = multiply()
+print(answer)  # 42.0
+
+# breakdown:
+# def on line 1 is define
+# followed by the name of the function 'multiply'
+# all functions have () after the name
+#if the function will take paremeters they're declared in parentheses
+#the parentheses are required, even when there are no parameters
